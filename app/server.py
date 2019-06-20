096870178a7970135c3b209db31e8062307b0fde
Susan Currie Sivek <susan.sivek@gmail.com>
2019-06-19 21:28:47 -0700

    editttt

--- app/server.py
+++ app/server.py
@@ -63,7 +63,7 @@ async def analyze(request):
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)
-    pred_phrase = ", ".join(preds)
+    pred_phrase = ", ".join(prediction)
     #preds = prediction.split(';')
     #if len(preds) > 1:
      #   pred_phrase = ", ".join(preds)
