341091c01c6bd80e02a4a98ca5d25e20400cbd14
Susan Currie Sivek <susan.sivek@gmail.com>
2019-06-20 09:21:18 -0700

    blurgh

--- app/server.py
+++ app/server.py
@@ -63,9 +63,7 @@ async def analyze(request):
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
-    pred_str = str(prediction)
-    pred = pred_str.split(' ')[1]
-    return JSONResponse({'result': str(pred)})
+    return JSONResponse({'result': str(prediction)})
     
 if __name__ == '__main__':
     if 'serve' in sys.argv:
