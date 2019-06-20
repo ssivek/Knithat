97868c0678beaed48e9cb4c9a63719ccd0001b2c
Susan Currie Sivek <susan.sivek@gmail.com>
2019-06-19 22:43:59 -0700

    last try tonight

--- app/server.py
+++ app/server.py
@@ -63,7 +63,7 @@ async def analyze(request):
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
-    return JSONResponse({'result': prediction})
+    return JSONResponse({'result': str(prediction)})
     
 if __name__ == '__main__':
     if 'serve' in sys.argv:
