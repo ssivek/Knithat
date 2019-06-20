346c9c79347f752af6afaefd00cce21f288e81e9
Susan Currie Sivek <susan.sivek@gmail.com>
2019-06-19 22:15:05 -0700

    full circle

--- app/server.py
+++ app/server.py
@@ -62,8 +62,7 @@ async def analyze(request):
     img_data = await request.form()
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
-    prediction = learn.predict(img)[0]
-    return JSONResponse({'result': prediction})
+    return JSONResponse({'result': learn.predict(img)[0]})
     
 if __name__ == '__main__':
     if 'serve' in sys.argv:
