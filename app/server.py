759b9387e7efd6a85897cbb1c1e516987bb67ecb
Susan Currie Sivek <susan.sivek@gmail.com>
2019-06-24 15:26:31 -0700

    Add files via upload

--- app/server.py
+++ app/server.py
@@ -127,5 +127,5 @@ async def analyze(request):
         #   return "Error: " + str(err)
 
 if __name__ == '__main__':
-if 'serve' in sys.argv:
+    if 'serve' in sys.argv:
         uvicorn.run(app=app, host='0.0.0.0', port=5000, log_level="info")
