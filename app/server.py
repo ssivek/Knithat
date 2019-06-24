0da88a57006503fd5923ad93a39776a57db3018b
Susan Currie Sivek <susan.sivek@gmail.com>
2019-06-24 10:13:42 -0700

    fixed split

--- app/server.py
+++ app/server.py
@@ -67,16 +67,16 @@ async def analyze(request):
     img_data = await request.form()
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
-    prediction = learn.predict(img) #[0] # returns "(MultiCategory ribbed, tensor([0., 0., 0...."
+    prediction = learn.predict(img) #[0] # returns "(MultiCategory ribbed;cables, tensor([0., 0., 0...."
     
     # transform prediction into API call to Ravelry using detected pattern attributes
     a = str(prediction) # makes list from prediction into a string
     b = a.split(',') # splits string on the commas
-    c = b[0] # extracts first item out of string "(MultiCategory ribbed"
-    d = c.split(' ') # splits item(s) on the space
-    e = d[1:] # extracts the second and all following items, which are pattern attributes
-    f = e.split(';') # splits item(s) on the semicolon
-    pattern_attributes = '&pa=' + '%2B'.join(f) # joins pattern attributes to add to API call string
+    c = b[0] # extracts first item out of string "(MultiCategory ribbed;cables, ..."
+    d = c.split(' ') # splits item(s) on the space (Multicategory, ribbed;cables)
+    e = d[1:] # extracts the second and all following items, which are pattern attributes, as a list with one item ['ribbed;cables']
+    f = e[0].split(';') # splits item(s) on the semicolon into a new list ['ribbed', 'cables']
+    pattern_attributes = '&pa=' + '%2B'.join(e) # joins pattern attributes to add to API call string
     params = '&' + pattern_attributes # + user_input if/when user input options eventually exist!
     api_url = str('https://api.ravelry.com/patterns/search.json?craft=knitting&photo=yes&pc=hat&sort=best&ratings=5&page=1' + str(params))
     
