cb7d16235e9932342d917e94189f5166e05e9acc
Susan Currie Sivek <susan.sivek@gmail.com>
2019-06-25 17:21:32 -0700

    try slice

--- app/static/client.js
+++ app/static/client.js
@@ -29,15 +29,15 @@ function analyze() {
   xhr.onload = function(e) {
     if (this.readyState === 4) {
         var response = JSON.parse(e.target.responseText);
-        var firstPatt = Object.keys(response)[0];
+        var firstPatt = response.slice(0,1);
         el('result-label').innerHTML = `
-        Result in ugly format: <br>${response['result']}
-        <br>How about this new var firstPatt; what does it do? ${firstPatt.info}
-        <br>Try for a pattern name:  ${response['result'][0].info}
-        <br>And a link:  ${response['result'][0].link}
-        <br>And a photo URL: ${response['result'][0].photo}
-        <br>And a free indicator: ${response['result'][0].free}
-        <br>Happy knitting!<br>
+          Result in ugly format: <br>${response['result']}
+          <br>How about this new var firstPatt; what does it do? Should be ugly here: ${firstPatt}
+          <br>Try for a pattern name:  ${response['result'][0].info}
+          <br>And a link:  ${response['result'][0].link}
+          <br>And a photo URL: ${response['result'][0].photo}
+          <br>And a free indicator: ${response['result'][0].free}
+          <br>Happy knitting!<br>
         `;
     }
     el('analyze-button').innerHTML = 'Analyze';
